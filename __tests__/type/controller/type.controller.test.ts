7b2665e5b48450087840cad27bea4a2448d544d6
Héctor Fierros <hector.fierros@wizeline.com>
2023-10-09 23:18:04 -0600

    Actualiza versiones de jest y corrije error en prueba de controlador

--- __tests__/type/controller/type.controller.test.ts
+++ __tests__/type/controller/type.controller.test.ts
@@ -7,17 +7,18 @@ jest.mock('../../../type/services/type.service')
 
 
 test('Test controller', async ()=> {
+        const mockedTypeService = jest.mocked(typeService)
+        mockedTypeService.getTriangleType = jest.fn()
         const req = getMockReq({body: {
                 sideA: 1,
                 sideB: 2,
                 sideC: 3
             }
         });
-    
         
         const {res}  = getMockRes();
     
-        jest.mocked(typeService.getTriangleType).mockResolvedValue('SCALENE');
+        jest.mocked(mockedTypeService.getTriangleType).mockResolvedValue('SCALENE');
     
         await typeController.getType(req, res);
         expect(res.status).toHaveBeenCalledWith(200);
