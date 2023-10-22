225b9f374ea2c500a09bd2b02b2b2a606c175fd3
Tonaram <gtonatiuhrp@gmail.com>
2023-10-21 23:54:25 -0600

    changed type.services.test.ts for 100

--- __tests__/type/services/type.services.test.ts
+++ __tests__/type/services/type.services.test.ts
@@ -2,8 +2,20 @@ import {expect, jest, test} from '@jest/globals';
 import { TriangleDto } from '../../../type/dto/triangle.dto';
 import typeService from '../../../type/services/type.service';
 
-test('test triangle service isosceles', () => {
-
+describe('Triangle Type Service', () => {
+  
+  test.each([
+    [{sideA: 2, sideB: 2, sideC: 2}, 'EQUILATERAL'],
+    [{sideA: 2, sideB: 3, sideC: 4}, 'SCALENE'],
+    [{sideA: 2, sideB: 2, sideC: 3}, 'ISOSCELES'],
+    [{sideA: 2, sideB: 3, sideC: 2}, 'ISOSCELES'],
+    [{sideA: 3, sideB: 2, sideC: 2}, 'ISOSCELES'],
+  ])('should return %s for triangle %o', async (triangle: TriangleDto, expectedType: string) => {
+    const result = await typeService.getTriangleType(triangle);
+    expect(result).toBe(expectedType);
+  });
+  
+  test('test triangle service isosceles', () => {
     const triangleDto: TriangleDto = {
         sideA: 2, 
         sideB: 2,
@@ -12,6 +24,6 @@ test('test triangle service isosceles', () => {
     
     typeService.getTriangleType(triangleDto).then(triangleType => {
         expect(triangleType).toBe('ISOSCELES');
-    })
-    
+    });
+  });
 });
